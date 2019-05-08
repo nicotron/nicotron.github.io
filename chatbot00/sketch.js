0882bfea9a46754f913fa67a5a156092f2684cc5
Nicolás Troncoso <hi@nicotron.cl>
2019-05-07 21:03:28 -0300

    online

--- chatbot00/sketch.js
+++ chatbot00/sketch.js
@@ -1,26 +1,42 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
 // Number Guessing Chatbot
 // Edited Video: https://www.youtube.com/watch?v=zGe1m_bLOFk
 function setup() {
   noCanvas();
 
   let bot = new RiveScript();
-  bot.loadFile("brain.rive", brainReady, brainError);
+  // bot.loadFile("brain/testsuite.rive").then(loading_done).catch(loading_error);
+  bot.loadFile("brain.rive").then(brainReady).catch(brainError);
 
   function brainReady() {
     console.log('Chatbot ready!');
     bot.sortReplies();
-  }
 
+    // send information from js, to brain.rive in order to manipulate the
+    // possible responses from the bot
+    // here is passing the let num
+    let num = floor(random(10)) + 1;
+    console.log(num);
+    let reply = bot.reply('local-user', 'set ' + num);
+
+    // RiveScript remembers user data by their username and can tell
+    // multiple users apart.
+    let username = 'local-user';
+
+    // NOTE: the API has changed in v2.0.0 and returns a Promise now.
+    bot.reply(username, "Hello, bot!")
+    .then(
+      function(reply) {
+      console.log('The bot says: ' + reply);
+      output.html(reply);
+      }
+    );
+  }
 
   function brainError() {
     console.log('Chatbot error!')
   }
 
-  let button = select('#submit'); //
+  let button = select('#submit');
   let user_input = select('#user_input');
   let output = select('#output');
 
@@ -28,7 +44,12 @@ function setup() {
 
   function chat() {
     let input = user_input.value();
-    let reply = bot.reply('local-user', input);
-    output.html(input);
+    bot.reply('local-user', input)
+    .then(
+      function(reply) {
+        console.log(reply);
+        output.html(reply);
+      }
+    );﻿
   }
 }
