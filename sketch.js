b405c9daed5a07c8bf2e2ce4e295eddff813169c
Nicolás Troncoso <hi@nicotron.cl>
2019-11-17 13:53:15 -0300

    keyboard setting

--- sketch.js
+++ sketch.js
@@ -1,61 +1,58 @@
-let txt, txt_Width_Window, index, txtSize, img, msg, indexM;
-let points = [-250, -250, 250, -250, 250, 250, -250, 250];
-let mazurquica;
-let pointer, vA, vB, vC, vD;  // vectors for the frame edit
+let txt, txt_Width_Window, index, txtSize, msg, indexM, x, y, setRewind;
+// let points = [-250, -250, 250, -250, 250, 250, -250, 250];
+let points = [0,0,500,0,500,500,0,500];
+let mazurquica, frame, goX, goY;
+let pointer;  // vectors for the frame edit
 
 function preload() {
   mazurquica = loadFont('Mazurquica-Medium.otf');
 }
 
 function setup() {
-  createCanvas(1024, 768, WEBGL);
-
-  txt_Width_Window = 200;
+  // createCanvas(1024, 768, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  frame = true;
+  goX= false;
+  goY= false;
+  setRewind = 40000;
+  txt_Width_Window = 250;
   txt = createGraphics(txt_Width_Window, 500);
-
+  y = 0;
   index = 0;
-  txtSize = 20;
-  img = loadImage('bg.jpeg');
+  txtSize = 200;
   indexM = 0;
   msg = [
-    'EL DERECHO DE VIVIR EN PAZ',
+    'EL DERECHO DE VIVIR EN PAZ\n\tEL DERECHO DE VIVIR EN PAZ',
     'NOS ESTAN MATANDO',
     'HASTA QUE VALGA LA PENA VIVIR',
     'DESDE CAMILO CATRILLANCA A GUSTAVO GATICA \n\nHAY 361 DIAS',
     'NUEVA CONSTITUCION',
-    'PROMETIMOS QUE NUNCA MÁS'
+    'PROMETIMOS QUE NUNCA MÁS',
+    'Miren el hervidero de vigilantes \n\npara rociarle flores al estudiante',
+    'miren como relumbran carabineros \n\npara ofrecerle premios a los obreros',
+    'miren como se visten cabo y sargento \n\npara teñir de rojo los pavimentos',
+    'miren como le muestran una escopeta \n\npara quitarle al pobre su marraqueta',
+    'NO HABRÁ PAZ MIENTRAS NO HAYA JUSTICIA'
   ]
   // console.log(indexM, msg.length);
 
+
   //vectors
   pointer = createVector(0, 0);
-  vA = createVector(-250, -250);
-  vB = createVector(  250, -250);
-  vC = createVector(  250, 250);
-  vD = createVector(-250, 250);
-
 }
 
 function draw() {
-
-  pointer.x = mouseX;
-  pointer.y - mouseY;
-
-  // next work, intruitive frame setting
-  // for(let i=0; i < points.length; i++){
-  //   if()
-  //
-  // }
-
-
+  translate(-windowWidth/2, -windowHeight/2);
   background(0);
+
   push();
   txt.background(0, 255);
   txt.textFont(mazurquica);
   txt.textSize(txtSize);
   txt.fill(255);
   txt.strokeWeight(0);
-  txt.text(msg[indexM], 0, 0, txt_Width_Window, 768);
+  // txt.text(msg[indexM].toUpperCase(), 0, y, txt_Width_Window, 1000);
+  txt.text(msg[indexM].toUpperCase(), y, txtSize);
   texture(txt);
   beginShape();
   vertex(points[0], points[1], 0, 0);
@@ -65,54 +62,70 @@ function draw() {
   endShape();
   pop();
 
-  strokeWeight(1);
-  stroke(120);
-  line(points[0], points[1], points[2], points[3]);
-  line(points[2], points[3], points[4], points[5]);
-  line(points[4], points[5], points[6], points[7]);
-  line(points[6], points[7], points[0], points[1]);
-  strokeWeight(2);
-  point(points[0], points[1]);
-  point(points[2], points[3]);
-  point(points[4], points[5]);
-  point(points[6], points[7]);
+  if(frame) {
+    strokeWeight(1);
+    stroke(120);
+    line(points[0], points[1], points[2], points[3]);
+    line(points[2], points[3], points[4], points[5]);
+    line(points[4], points[5], points[6], points[7]);
+    line(points[6], points[7], points[0], points[1]);
+    strokeWeight(2);
+    point(points[0], points[1]);
+    point(points[2], points[3]);
+    point(points[4], points[5]);
+    point(points[6], points[7]);
+  }
+  // console.log(y, -(points[5]-points[3]));
+  if(y < -setRewind) {
+    y = 0;
+  }
 
+  if(goX) {x-=5;}
+  if(goY) {y-=5;}
 
-  //
-  //
-  // beginShape();
-  // strokeWeight(3);
-  // stroke(250);
-  // vertex(points[0], points[1]);
-  // vertex(points[2], points[3]);
-  // vertex(points[4], points[5]);
-  // vertex(points[6], points[7]);
-  // endShape();
-  //
-  // if(second()%5===0) {
-  //   console.log(second());
-  //   if (indexM > msg.length - 1) {
-  //     indexM = 0;
-  //     break;
-  //   }
-  //   indexM = indexM + 1;
-  //   break;
-  // }
+  for ( i = 0; i < points.length; i+= 2) {
+    let d = dist(mouseX,mouseY,points[i],points[i+1]);
+    if(d < 20) {
+      // function mouseDragged() {
+        points[i] = mouseX;
+        points[i+1] = mouseY;
+      // }
+      line(mouseX,mouseY,points[i],points[i+1]);
 
+    }
+  }
 }
+
+
 
 function mousePressed() {
   if (index > 7) {
     index = 0;
   }
-  points[index] = mouseX - width / 2;
-  points[index + 1] = mouseY - height / 2;
+  points[index] = mouseX;
+  points[index + 1] = mouseY;
   // console.log(index, points[index], points[index + 1], points);
 
   index += 2;
 }
 
 function keyPressed() {
+  if (key === ' ') {
+    frame = !frame;
+  }
+  if (key === 'x') {
+    goX = !goX;
+  }
+  }
+  if (key === 'y') {
+    goY = !goY;
+  }
+  if (key === 'r') {
+    setRewind = y*-1;
+  }
+  if (key === 'e') {
+    setRewind = 4000;
+  }
   if (keyCode === UP_ARROW) {
     txtSize = txtSize + 1;
   } else if (keyCode === DOWN_ARROW) {
@@ -135,16 +148,4 @@ function keyPressed() {
     }
   }
   return false; // prevent default
-}
-
-function keyTyped() {
-  if (key === 'a') {
-    // console.log(indexM, msg.length);
-    if (indexM > msg.length - 1) {
-      indexM = 0;
-    }
-    indexM++;
-  }
-  // uncomment to prevent any default behavior
-  // return false;
 }
