487bfa2e82cd342fe6c788496e8e709de9d3e0ac
Nicolás Troncoso <hi@nicotron.cl>
2019-11-26 19:37:11 -0300

    update, cv publish

--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ let txt, txt_Width_Window, index, txtSize, msg, indexM, x, y, setRewind, wX, wY,
 // let points = [-250, -250, 250, -250, 250, 250, -250, 250];
 let points = [
   0, 0,
-  250, 50,
+  // 250, 50,
   500, 0,
+  // 500, 250,
   500, 500,
-  250, 450,
+  // 0, 250,
+  // 250, 450,
   0, 500
 ];
 let mazurquica, frame, goX, goY, gifGo;
@@ -21,8 +23,8 @@ function preload() {
 }
 
 function setup() {
-  wX = 1600;
-  wY = 1200;
+  wX = 800;
+  wY = 600;
   // createCanvas(1024, 768, WEBGL);
   let p5vancas = createCanvas(wX, wY, WEBGL);
   canvas = p5vancas.canvas;
@@ -55,19 +57,35 @@ function setup() {
     'miren como le muestran una escopeta \n\npara quitarle al pobre su marraqueta',
     'NO HABRÁ PAZ MIENTRAS NO HAYA JUSTICIA',
     'NO VAMOS A PARAR',
-    'Quieres justicia? @sebastianpinera entrégate.'
+    'Quieres justicia? @sebastianpinera entrégate.',
+    'Amnistía Internacional denuncia que el Ejército y Carabineros "han llevado a cabo ataques generalizados usando la fuerza de manera innecesaria y excesiva" con la intención de "dañar y castigar" a manifestantes durante crisis social en Chile.',
+    "Un acuerdo por la paz y contra la democracia"
   ]
-  // console.log(indexM, msg.length);
+  /*
+  José Miguel Uribe (Curicó),
+  Álex Núñez (Maipú),
+  Kevin Gómez y Romario Veloz (Coquimbo)
+  Manuel Rebolledo (atropellado en Talcahuano).
+  Paula Lorca y Alicia Cofré (encontrados en Líder de San Bernardo)
+  José Atilio Arancibia y Eduardo Caro (encontrados en Construmart de La Pintana)
+  Renzo Barbosa (encontrados en Líder de Quinta Normal)
+  Manuel Muga, Andrés Ponce, Yoshua Osorio y Julián Pérez (encontrados en empresa Kayser)
+  Cardenio Prado y Joel Triviño (atropello múltiple en San Pedro de La Paz)
+  Agustín Coro (impacto de bala tras saqueo en Puente Alto)
+  Valeska Carmona (impacto de bala en El Bosque)
+  Mariana Díaz (víctima de supuesta “bala loca”)
+  Daniela Carrasco (encontrada ahorcada en vía pública)
+  */
 
 
   //vectors
-  pointer = createVector(0, 0);
+  // pointer = createVector(0, 0);
 }
 
 function draw() {
   // translate(-windowWidth/2, -windowHeight/2);
   translate(-wX / 2, -wY / 2);
-  background(0);
+  background(255);
 
 
   if (gif) {
@@ -75,21 +93,46 @@ function draw() {
   }
 
   push();
-  txt.background(0, 255);
+  txt.background(255);
   txt.textFont(mazurquica);
   txt.textSize(txtSize);
-  txt.fill(255);
-  txt.strokeWeight(0);
-  // txt.text(msg[indexM].toUpperCase(), 0, y, txt_Width_Window, 1000);
+  // txt.strokeWeight(0.2)
+  // txt.stroke(255)
+  txt.fill(51);
+  txt.noStroke();
+  // txt.strokeWeight(0);
+  // txt.text(msg[indexM].toUpperCase(), x, y, txt_Width_Window, 1000);
   txt.text(msg[indexM].toUpperCase(), x, y + txtSize);
+
+  // texture(txt);
+
+
   texture(txt);
+  // translate(mouseX, mouseY);
+  // fill(255, 0, 0)
+  // strokeWeight(1)
   beginShape();
   vertex(points[0], points[1], 0, 0);
-  vertex(points[4], points[53], 250, 50);
   vertex(points[2], points[3], 500, 0);
-  vertex(points[6], points[7], 500, 500);
-  vertex(points[8], points[9], 250, 500);
-  vertex(points[10], points[11], 0, 500);
+  vertex(points[4], points[5], 500, 500);
+  vertex(points[6], points[7], 0, 500);
+  // vertex(points[8], points[9], 0, 500);
+  // vertex(points[10], points[11], 0, 250);
+
+
+  // vertex(0,0 , 0, 0);
+  // vertex(250, 0, 250, 0);
+  // vertex(500, 0 , 500, 0);
+  //
+  // vertex(mouseX+500, mouseY, 500, 250)
+  //
+  // vertex(500, 500 , 500, 500);
+  // vertex(250, 500, 250, 500);
+  //
+  // vertex(0, 500 , 0, 500);
+  // vertex(mouseX, mouseY, 0, 250)
+  // vertex(0,250 , 0, 250);
+
   endShape();
   pop();
 
@@ -108,15 +151,15 @@ function draw() {
 
   if (frame) {
 
-    stroke(120);
+    stroke(0);
     for (let i = 0; i < points.length; i += 2) {
-      strokeWeight(2 + (i * 2));
+      strokeWeight(2);
       // line(points[i], points[i + 1], points[i + 2], points[i + 3]);
       // strokeWeight(2);
       point(points[i], points[i + 1]);
-      // point(points[2], points[3]);
-      // point(points[4], points[5]);
-      // point(points[6], points[7]);
+      point(points[2], points[3]);
+      point(points[4], points[5]);
+      point(points[6], points[7]);
 
     }
   } // console.log(y, -(points[5]-points[3]));
@@ -128,10 +171,10 @@ function draw() {
   }
 
   if (goX) {
-    x -= 3;
+    x -= 5;
   }
   if (goY) {
-    y -= 3;
+    y -= 5;
   }
   if (gifGo) {
     gifLength++;
@@ -147,7 +190,7 @@ function draw() {
       // }
       noFill();
       strokeWeight(1)
-      stroke(255, 255, 0);
+      stroke(0);
       circle(points[i], points[i + 1], 50);
 
     }
@@ -197,9 +240,9 @@ function keyPressed() {
     setRewind = 4000;
   }
   if (keyCode === UP_ARROW) {
-    txtSize = txtSize + 1;
+    txtSize = txtSize + 10;
   } else if (keyCode === DOWN_ARROW) {
-    txtSize = txtSize - 1;
+    txtSize = txtSize - 10;
   }
   if (keyCode === RIGHT_ARROW) {
     // console.log(indexM, msg.length);
